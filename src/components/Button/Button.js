08112af6cb0d6e53cbc63bdc3b738923f78a7595
Tanmoy <taijulislam.st9@gmail.com>
2020-02-26 11:19:19 +0600

    Button design fixed

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,24 +3,24 @@ import React from "react";
 function Button({ loadNextFour, quickLoadItems, lazyLoadItems, navigateFirstPage }) {
   return (
     <div className="row mt-3">
-      <div className="col">
+      <div className="direction-button">
         <button className="btn btn-danger">Reload Current Page</button>
       </div>
-      <div className="col">
+      <div className="direction-button">
         <button onClick={() => navigateFirstPage()} className="btn btn-danger">Navigate First Page</button>
       </div>
-      <div className="col">
-        <button className="btn btn-success" onClick={() => loadNextFour()}>
+      <div className="direction-button">
+        <button className="btn btn-custom" onClick={() => loadNextFour()}>
           Navigate Next 4 Items
         </button>
       </div>
-      <div className="col">
-        <button onClick={() => quickLoadItems()} className="btn btn-info">
+      <div className="direction-button">
+        <button onClick={() => quickLoadItems()} className="btn btn-custom">
           Quick Load More Items
         </button>
       </div>
-      <div className="col">
-        <button onClick={() => lazyLoadItems()} className="btn btn-warning">
+      <div className="direction-button">
+        <button onClick={() => lazyLoadItems()} className="btn btn-custom">
           Lazy Load More Items
         </button>
       </div>
